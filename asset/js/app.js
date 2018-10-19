53be09b24be1d4783bf655018921cf3fcf536558
CesarRosales16 <00060917@uca.edu.sv>
2018-10-19 10:54:29 -0600

    falta validacion

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,22 +1,21 @@
 
-const navSlide = ()=>{
-    const burger = document.querySelector(".burger"); 
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener("click", ()=>{
+    burger.addEventListener("click", () => {
         nav.classList.toggle('nav-active');
 
-        navLinks.forEach((link, index) =>{
-            
-            if(link.style.animation)
-            {
+        navLinks.forEach((link, index) => {
+
+            if (link.style.animation) {
                 link.style.animation = '';
             }
-            else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+0.3}s`;
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
-            
+
         });
         burger.classList.toggle('toggle');
     });
@@ -25,4 +24,63 @@ const navSlide = ()=>{
 
 navSlide();
 /*********************************Colocar aca el desarrollo de su ejercicio***************************/
+//1
+var cont = 1;
+//2
+var bitacoras = [];
+//3
+var formulario = document.getElementById("bitacora");
+//4
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    let bitacora = {
+        cant: cont,
+        fecha: formulario[1].value,
+        descripcion: formulario[2].value,
+        cantidad: formulario[3].value
+    }
+    bitacoras.push(bitacora);
+    cont++;
+    mostrar();
+});
+//5
+const crearElemento = (bitacora, tbody) => {
+    let tr = document.createElement("tr");
+    Object.values(bitacora).forEach(item => {
+        let td = document.createElement("td");
+        let content = document.createTextNode(item);
+        td.appendChild(content);
+        tr.setAttribute("class", "click");
+        tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+}
+//6
+const eliminar = (tbody) => {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+}
+//7
+const agregar = () => {
+    var eventtr = document.querySelectorAll(".click");
+    eventtr.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            document.querySelector("#fecha").value = item.childNodes[1].textContent;
+            document.querySelector("#descp").value = item.childNodes[2].textContent;
+            document.querySelector("#cant").value = item.childNodes[3].textContent;
+        });
+    })
+}
+//8
+const mostrar = () => {
+    if (document.querySelector(".tabla-btc tbody").childElementCount > 0) {
+        eliminar(document.querySelector(".tabla-btc tbody"));
+    }
+    bitacoras.forEach(item => {
+        crearElemento(item, document.querySelector(".tabla-btc tbody"));
+    });
+    agregar();
+} 
 
+//VALIDACION
